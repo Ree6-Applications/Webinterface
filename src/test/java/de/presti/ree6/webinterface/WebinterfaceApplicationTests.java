e84319fe63d9726fbcfd3f71fac56f651f4b195c
DxsSucuk <dxssucuk@hotmail.com>
2021-12-23 12:48:13 +0100

    v2.0
    = Code cleanup.

--- src/test/java/de/presti/ree6/webinterface/WebinterfaceApplicationTests.java
+++ src/test/java/de/presti/ree6/webinterface/WebinterfaceApplicationTests.java
@@ -1,14 +1,7 @@
 package de.presti.ree6.webinterface;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WebinterfaceApplicationTests {
-
-    @Test
-    void contextLoads() {
-        Server.getInstance().logger.warn("Please do not use Test mode!");
-    }
-
 }
