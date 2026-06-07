6c9287c08069b99133b3dc3710761999277d3ca4
endercheststudio <enderchest.studio1@gmail.com>
2026-06-07 14:02:06 -0700

    Update constants.ts
    Signed-off-by: endercheststudio <enderchest.studio1@gmail.com>

--- Frontend/src/lib/scripts/constants.ts
+++ Frontend/src/lib/scripts/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_PATH = import.meta.env.VITE_API_URL || "https://api.ree6.de" // <- Edit this string to your own API
+export const BASE_PATH = import.meta.env.VITE_API_URL || "https://backend-setup.enderchest.studio" // <- Edit this string to your own API
 export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://invite.ree6.de" // <- Edit this string to your Invite link
 
 export function get(path: string) {
